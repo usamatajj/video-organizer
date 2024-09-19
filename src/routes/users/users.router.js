ecefc7bededf70dd5b867f2cb2911b53674c0da1
usamatajj <usama.taj@hotmail.com>
2024-09-19 11:21:50 +0500

    added users model and configurations

--- src/routes/users/users.router.js
+++ src/routes/users/users.router.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
-const { getAllUsers } = require('./users.controller');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('./users.controller');
 
 const usersRouter = express.Router();
 
+// GET /users
 usersRouter.get('/', getAllUsers);
+
+// GET /users/:id
+usersRouter.get('/:id', getUserById);
+
+// POST /users
+usersRouter.post('/', createUser);
+
+// PUT /users/:id
+usersRouter.put('/:id', updateUser);
+
+// DELETE /users/:id
+usersRouter.delete('/:id', deleteUser);
 
 module.exports = usersRouter;
